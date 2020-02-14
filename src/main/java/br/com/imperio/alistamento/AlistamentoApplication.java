73e44b30a24fba37270ef9c36419a037bea406df
Fabio Araujo <fabio_araujo11@hotmail.com>
2020-02-14 11:56:18 -0300

    Adicionado Swagger

--- src/main/java/br/com/imperio/alistamento/AlistamentoApplication.java
+++ src/main/java/br/com/imperio/alistamento/AlistamentoApplication.java
@@ -3,7 +3,10 @@ package br.com.imperio.alistamento;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class AlistamentoApplication {
 
 	public static void main(String[] args) {
